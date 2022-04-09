aea373343a793f0e879d5c746fd0fd4e4f355bfd
Bryson Kern <kernbryson@yahoo.com>
2022-04-09 19:17:12 -0400

    added ratings

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -1,9 +1,7 @@
 let imdbUrl = "https://imdb-api.com/en/API/Search/k_w0qs6e0f/";
 let imdbKey = "k_w0qs6e0f";
-let rottenUrl =
-  "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=";
-let rottenKey = "bpmqxh5zby5g7xkdqqhsfypq";
-let tmdUrl = "https://api.themoviedb.org/3/search/movie?";
+// let movieDescUrl =
+//   "https://imdb-api.com/en/API/Report/k_w0qs6e0f/" + movieId + "/Wikipedia,";
 let searchValue = "";
 let searchBtn = document.querySelector(".search-btn");
 let searchInput = document.querySelector(".search-input");
@@ -14,7 +12,6 @@ function search(searchValue) {
   searchValue = searchInput.value;
   console.log(searchValue);
   getMovies(searchValue);
-  getRotten(searchValue);
 }
 function getMovies(searchValue) {
   fetch(imdbUrl + searchValue)
@@ -43,17 +40,44 @@ function getMovies(searchValue) {
       ulMovie.append(list);
       list.append(imageList);
       console.log(moviePoster);
+      let movieId = data["results"]["0"]["id"];
 
       console.log(title);
+      //   getDesc(movieId, list);
+      getReviews(movieId, list);
     });
-  //   let list = document.createElement("li");
-  //   let imageList = document.createElement("img");
-  //   let moviePoster = data["results"]["0"]["image"];
 }
-function getTMDB() {
-  fetch(rottenUrl + rottenKey)
+// function getDesc(movieId, list) {
+//   fetch("https://imdb-api.com/en/API/Wikipedia/k_w0qs6e0f/" + movieId)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+//       let description = document.createElement("h2");
+//       let movieDesc = data["plotShort"]["plainText"];
+//       description.classList.add("moviedescription");
+//       description.textContent = movieDesc;
+//       list.append(description);
+//     });
+// }
+function getReviews(movieId, list) {
+  fetch("https://imdb-api.com/en/API/MetacriticReviews/k_w0qs6e0f/" + movieId)
     .then((response) => response.json())
     .then((data) => {
+      let publisherdata = data["items"]["0"]["publisher"];
+      let reviewdata = data["items"]["0"]["content"];
+      let review = document.createElement("p");
+      review.textContent = reviewdata;
+      list.append(review);
+      let movieScoredata = data["items"]["0"]["rate"];
+      let movieScore = document.createElement("p");
+      movieScore.textContent = movieScoredata + "/100";
+      list.append(movieScore);
+      let publisher = document.createElement("p");
+      publisher.textContent = publisherdata;
+      list.append(publisher);
       console.log(data);
+      review.classList.add("review");
+      movieScore.classList.add("moviescore");
+      publisher.classList.add("publisher");
     });
 }
