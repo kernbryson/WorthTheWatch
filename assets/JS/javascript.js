867818d46d1cd56d4b7af57af21768dbd6800138
tony <tonywatkins2014@gmail.com>
2022-04-12 21:23:48 -0400

    js

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -1,6 +1,5 @@
-let imdbUrl = "https://imdb-api.com/en/API/Search/k_w0qs6e0f/";
-let imdbGenreUrl = "https://imdb-api.com/API/AdvancedSearch/k_w0qs6e0f?genres="
 
+let imdbUrl = "https://imdb-api.com/en/API/Search/k_w0qs6e0f/";
 let imdbKey = "k_w0qs6e0f";
 let rottenUrl =
   "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=";
@@ -8,51 +7,17 @@ let rottenKey = "bpmqxh5zby5g7xkdqqhsfypq";
 let searchValue = "";
 let searchBtn = document.querySelector(".search-btn");
 let searchInput = document.querySelector(".search-input");
-
-$(".button").click(search);
-
-function search(event) {
-  genre=$(event.target)[0].getInnerHTML();
-  if(genre == "Search")
-    genre = null;
+searchBtn.addEventListener("click", search);
+function search(searchValue) {
   searchValue = searchInput.value;
   console.log(searchValue);
-  getMovies(genre, searchValue);
+  getMovies(searchValue);
 }
-
-function getMovies(genre, searchValue) {
-  
-  var finalUrl = "";
-  if(genre != null && searchValue != null && searchValue != ""){
-    finalUrl = imdbGenreUrl + genre+ "&title=" + searchValue + "&title_type=feature" ;
-  } else if(genre != null){
-    finalUrl = imdbGenreUrl + genre  + "&title_type=feature";
-  } else {
-    finalUrl = imdbUrl + searchValue  + "&title_type=feature";
-  }
-  fetch(finalUrl)
+function getMovies(searchValue) {
+  fetch(imdbUrl + searchValue)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      displayMovies(data.results);
-    })  
-    .catch(() => console.log("Error"));
+    });
 }
 
-function displayMovies(resultsMovie) {
-  for (var i = 1; i < resultsMovie.length; i++) {
-      var card = $("<div>").addClass("card");      
-      var title = $("<div>").addClass("card-header h5").text(resultsMovie[i].title);
-      var figure = $("<figure>").addClass("image is-128x128");
-      var imgIcon = $("<img>").attr("src", resultsMovie[i].image);
-      figure.append(imgIcon);
-      
-      var genre = $("<p>").addClass("card-text").text("Genre : " + resultsMovie[i].genres);
-      var rating = $("<p>").addClass("card-text").text("Rating : " + resultsMovie[i].imDbRating);
-      var plot = $("<p>").addClass("card-text").text("Plot : " + resultsMovie[i].plot);
-      var cardBody = $("<div>").addClass("card-body");
-      cardBody.append(title, genre, rating, plot, figure);
-      card.append(cardBody);
-      $("#allMovies").append(card);
-  }
-};
