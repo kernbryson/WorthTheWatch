16e03c25be9d81456e9bdb15a163ae4784144670
dewey preast <webwalkin@me.com>
2022-04-14 22:33:38 -0400

    updating readme

--- README.md
+++ README.md
@@ -2,31 +2,34 @@
 
 # Worth the Watch
  Seeing that everyone is looking to watch a movie though there are so many choices it is nice to have an easy way to search and look up the movies either by genre or title.
-
+    
 # Please join us in searching for a title you recently heard of
 
-[](https://github.com/kernbryson/WorthTheWatch.git)
+-Check out our source code at
+[worththewatch](https://github.com/kernbryson/WorthTheWatch.git)
 # You definately need to see the trailer to figure out if it's Worth the Watch.
 
 # using youtube we can provide trailers to accompany your search
 
-[youtubeApi](https://developers.google.com/youtube/v3)
+-[youtubeApi](https://developers.google.com/youtube/v3)
 ## <p align="center">
-## [![Brysons GitHub stats](https://github-readme-stats.vercel.app/api?username=kernbryson)](https://github.com/kernbryson/github-readme-stats)
+## [![Brysons GitHub stats](https://github-readme-stats.vercel.app/api?username=kernbryson)](https://github.com/kernbryson/github-readme-stats#gh-dark-mode-only)
 
 
-# using imdb the amount of information for movies you are interested in seems limitless
-
-## [imdb](https://imdb-api.com/api)
+# using [imdb](https://imdb-api.com/api) the amount of information for movies you are interested in seems limitless
 
 
 ## Some of the things we intend to do in the future include following through all the way to paid viewing options.
-    + To include sponsorship throughout all aspects of film memorabilia to include but not be limited to the official film poster other types of clothing and possibly even some fanpage sites. 
-    + Social media links to actors sites can be a further feature to add some user interactivity.
 
+* To include sponsorship throughout all aspects of film memorabilia to include but not be limited to the official film poster other types of clothing and possibly even some fanpage sites. 
+* Social media links to actors sites can be a further feature to add some user interactivity.
+
+## These were the objectives for our project
+
+-[x] to use two seperate apis
 # We have used the following tools in creating this project
-## ![](https://img.shields.io/badge/Code-JavaScript-informational?style=flat&logo=JavaScript&color=F7DF1E)
-## ![](https://img.shields.io/badge/Code-HTML5-informational?style=flat&logo=HTML5&color=E34F26)
+
+![Visual Studio Code](https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white)![](https://img.shields.io/badge/Code-JavaScript-informational?style=flat&logo=JavaScript&color=F7DF1E)-![](https://img.shields.io/badge/Code-HTML5-informational?style=flat&logo=HTML5&color=E34F26)![Freelancer](https://img.shields.io/badge/Freelancer-29B2FE?style=for-the-badge&logo=Freelancer&logoColor=white)
 ## <p align="right">
 contributors include Bhavna Jain, Bryson Kern, Dewey Preast and Tony Watkins
 <p align="left">
--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -1,36 +1,21 @@
 let imdbUrl = "https://imdb-api.com/en/API/Search/k_w0qs6e0f/";
 let imdbGenreUrl = "https://imdb-api.com/API/AdvancedSearch/k_w0qs6e0f?genres=";
-let imdbGenre_title_Url = "https://imdb-api.com/API/AdvancedSearch/k_w0qs6e0f?title=";
+let imdbGenre_title_Url =
+  "https://imdb-api.com/API/AdvancedSearch/k_w0qs6e0f?title=";
 
 let imdbKey = "k_w0qs6e0f";
-let finalArray = [];
-let movieArray = ["1", "2", "3", "4", "5", "6", "7", "8"];
-// let reviewArray = [];
-let movies = "";
+let rottenUrl =
+  "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=";
+let rottenKey = "bpmqxh5zby5g7xkdqqhsfypq";
 let searchValue = "";
 let searchBtn = document.querySelector(".search-btn");
 let searchInput = document.querySelector(".search-input");
-let ulMovie = document.querySelector("#movielist");
-let reviewApi = "";
-searchBtn.addEventListener("click", search);
-function search(searchValue) {
-  searchValue = searchInput.value;
-  console.log(searchValue);
-  getMovies(searchValue);
-  youtube(searchValue);
-}
-function getMovies(searchValue) {
-  var obj = {};
-
-  fetch(imdbUrl + searchValue)
 
 $(".button").click(search);
 
 function search(event) {
-  
-  genre=$(event.target)[0].getInnerHTML();
-  if(genre == "Search")
-    genre = null;
+  genre = $(event.target)[0].getInnerHTML();
+  if (genre == "Search") genre = null;
   searchValue = searchInput.value;
   console.log(searchValue);
   $("#allMovies").empty();
@@ -38,151 +23,102 @@ function search(event) {
 }
 
 function getMovies(genre, searchValue) {
-  
   var finalUrl = "";
-  if(genre != null && searchValue != null && searchValue != ""){
-    finalUrl = imdbGenreUrl + genre+ "&title=" + searchValue ;//+ "&title_type=feature" ;
-  } else if(genre != null){
-    finalUrl = imdbGenreUrl + genre ;// + "&title_type=feature";
+  if (genre != null && searchValue != null && searchValue != "") {
+    finalUrl = imdbGenreUrl + genre + "&title=" + searchValue; //+ "&title_type=feature" ;
+  } else if (genre != null) {
+    finalUrl = imdbGenreUrl + genre; // + "&title_type=feature";
   } else {
-    finalUrl = imdbGenre_title_Url + searchValue;//  + "&title_type=feature";
+    finalUrl = imdbGenre_title_Url + searchValue; //  + "&title_type=feature";
   }
   fetch(finalUrl)
     .then((response) => response.json())
     .then((data) => {
-      //get movie title
-      // movies = "";
-      for (i = 0; i < 6; i++) {
-        console.log(data);
-        let list = document.createElement("div");
-        let title = data["results"][movieArray[i]]["title"];
-        let titleName = document.createElement("h1");
-        titleName.classList.add("movietitle");
-        list.append(titleName);
-        titleName.textContent = title;
-        // //get movie date
-        let movieDate = document.createElement("h2");
-        movieDate.classList.add("movierelease");
-        list.append(movieDate);
-        // //get movie image
-        let link = document.createElement("a")
-        link.setAttribute("href","./pages/movies.html")
-        let imageList = document.createElement("img");
-        let moviePoster = data["results"][movieArray[i]]["image"];
-        imageList.classList.add("picture");
-        imageList.src = moviePoster;
-        ulMovie.append(list);
-        list.append(imageList);
-        // console.log(moviePoster);
-        let movieId = data["results"]["0"]["id"];
-      }
-      // console.log(title);
-      // obj.releaseDate = data["results"]["0"]["description"];
-      // obj.title = data["results"]["0"]["title"];
-      // obj.image = data["results"]["0"]["image"];
-      // finalArray.push(obj);
-
-      // for (i = 0; i < finalArray.length; i++) {
-      //   movies += "<li class='movietitle'>" + finalArray[i].title + "</li>";
-      //   movies +=
-      //     "<li class='moviepiclist'>" +
-      //     "<img class='picture' src='" +
-      //     finalArray[i].image +
-      //     "'>" +
-      //     "</li>";
-      //  movies +="<button class='learnmorebtn'>" + "learn more!" + "</button>");
-    });
-  //   getDesc(movieId, list);
-  // getReviews(movieId, movies);
-}
-
-// function getDesc(movieId, list) {
-//   fetch("https://imdb-api.com/en/API/Wikipedia/k_w0qs6e0f/" + movieId)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log(data);
-//       let description = document.createElement("h2");
-//       let movieDesc = data["plotShort"]["plainText"];
-//       description.classList.add("moviedescription");
-//       description.textContent = movieDesc;
-//       list.append(description);
-//     });
-// }
-// function getReviews(movieId, movies) {
-//   var obj = {};
-
-//   fetch("https://imdb-api.com/en/API/MetacriticReviews/k_w0qs6e0f/" + movieId)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log(data);
-//       // let publisherdata = data["items"]["0"]["publisher"];
-//       // let reviewdata = data["items"]["0"]["content"];
-//       // let review = document.createElement("p");
-//       // review.textContent = reviewdata;
-//       // list.append(review);
-//       // let movieScoredata = data["items"]["0"]["rate"];
-//       // let movieScore = document.createElement("p");
-//       // movieScore.textContent = movieScoredata + "/100";
-//       // list.append(movieScore);
-//       // let publisher = document.createElement("p");
-//       // publisher.textContent = publisherdata;
-//       // list.append(publisher);
-//       // console.log(data);
-//       // review.classList.add("review");
-//       // movieScore.classList.add("moviescore");
-//       // publisher.classList.add("publisher");
-//       // console.log(movieId);
-
-//       obj.publisher = data["items"]["0"]["publisher"];
-//       obj.reviewdata = data["items"]["0"]["content"];
-//       obj.movieScoredata = data["items"]["0"]["rate"];
-//       reviewArray.push(obj);
-//       for (i = 0; i < reviewArray.length; i++) {
-//         movies +=
-//           "<li class='review'>" + reviewArray[i].reviewdata + "/100" + "</li>";
-//         movies += "<li class='publisher'>" + reviewArray[i].publisher + "</li>";
-//         movies +=
-//           "<li class='moviescore'>" + reviewArray[i].movieScoredata + "</li>";
-//       }
-//       ulMovie.innerHTML = movies;
-//     });
-// }
-// bringing in the youtube api to include trailers as a feature in our returned movie search results
-let ytApiKey = "AIzaSyDiOCYClrpn3Cz3dIBf7MCWsD-e9KltnIE";
-function youtube(searchValue) {
-  fetch(
-    "https://youtube.googleapis.com/youtube/v3/search?key=" +
-      ytApiKey +
-      "&type=video&part=snippet&q=" +
-      searchValue +
-      "trailer"
-  )
-    .then((response) => response.json())
-    .then((data) => {
       console.log(data);
       displayMovies(data.results);
-    })  
+    })
     .catch(() => console.log("Error"));
 }
-// creating a card that will display the movies based on the value of i (returned results)
-function displayMovies(resultsMovie) {
-  for (var i = 1; i < resultsMovie.length; i++) {
-      var card = $("<div>").addClass("card");      
-      var title = $("<div>").addClass("card-header h5").text(resultsMovie[i].title);
-      var figure = $("<figure>").addClass("image is-128x128");      
-      var imgIcon = $("<img>").attr("src", resultsMovie[i].image); 
-      var link = $("<a>").attr("href", "https://www.google.com");
-    
-      figure.append(imgIcon);
 
-      // appending the different fields to return the results from the movie search
-      
-      var genre = $("<p>").addClass("card-text").text("Genre : " + resultsMovie[i].genres);
-      var rating = $("<p>").addClass("card-text").text("Rating : " + resultsMovie[i].imDbRating);
-      var plot = $("<p>").addClass("card-text").text("Plot : " + resultsMovie[i].plot);
-      var cardBody = $("<div>").addClass("card-body");
-      cardBody.append(title, genre, rating, plot, figure,link);
-      card.append(cardBody);
-      $("#allMovies").append(card);
+function displayMovies(resultsMovie) {
+  for (var i = 0; i < resultsMovie.length; i++) {
+    var card = $("<div>").addClass("card");
+    var title = $("<div>")
+      .addClass("card-header h5")
+      .text(resultsMovie[i].title);
+
+    var figure = $("<figure>").addClass("image is-128x128");
+    var imgIcon = $("<img>").attr("src", resultsMovie[i].image);
+    figure.append(imgIcon);
+    var myId = resultsMovie[i].id;
+    imgIcon.wrap(
+      "<a id='a" + myId + "' href=" + "./movies.html?id=" + myId + "></a>"
+    );
+    //var link = $("#a"+ myId).attr({href:},{id:resultsMovie[i].id});
+
+    var genre = $("<p>")
+      .addClass("card-text")
+      .text("Genre : " + resultsMovie[i].genres);
+    var rating = $("<p>")
+      .addClass("card-text")
+      .text("Rating : " + resultsMovie[i].imDbRating);
+    var plot = $("<p>")
+      .addClass("card-text")
+      .text("Plot : " + resultsMovie[i].plot);
+    var cardBody = $("<div>").addClass("card-body");
+    cardBody.append(title, genre, rating, plot, figure);
+    card.append(cardBody);
+    $("#allMovies").append(card);
   }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Functions to open and close a modal
+  function openModal($el) {
+    $el.classList.add("is-active");
+  }
+
+  function closeModal($el) {
+    $el.classList.remove("is-active");
+  }
+
+  function closeAllModals() {
+    (document.querySelectorAll(".modal") || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+
+  // Add a click event on buttons to open a specific modal
+  (document.querySelectorAll(".js-modal-trigger") || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+    console.log($target);
+
+    $trigger.addEventListener("click", () => {
+      openModal($target);
+    });
+  });
+
+  // Add a click event on various child elements to close the parent modal
+  (
+    document.querySelectorAll(
+      ".modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button"
+    ) || []
+  ).forEach(($close) => {
+    const $target = $close.closest(".modal");
+
+    $close.addEventListener("click", () => {
+      closeModal($target);
+    });
+  });
+
+  // Add a keyboard event to close all modals
+  document.addEventListener("keydown", (event) => {
+    const e = event || window.event;
+
+    if (e.keyCode === 27) {
+      // Escape key
+      closeAllModals();
+    }
+  });
+});
