ea0b66cf6da61c4fcf0b7d4da42ce77c4071a06b
Bryson Kern <kernbryson@yahoo.com>
2022-04-14 21:55:22 -0400

    finished functionality of movies page

--- assests/js/javascript.js
+++ assests/js/javascript.js
@@ -1,7 +1,6 @@
-let imdbUrl = "https://imdb-api.com/en/API/SearchMovie/k_w0qs6e0f/";
+let imdbUrl = "https://imdb-api.com/en/API/Search/k_w0qs6e0f/";
 let imdbGenreUrl = "https://imdb-api.com/API/AdvancedSearch/k_w0qs6e0f?genres=";
-let imdbGenre_title_Url =
-  "https://imdb-api.com/API/AdvancedSearch/k_w0qs6e0f?title=";
+let imdbGenre_title_Url = "https://imdb-api.com/API/AdvancedSearch/k_w0qs6e0f?title=";
 
 let imdbKey = "k_w0qs6e0f";
 let rottenUrl =
@@ -14,8 +13,10 @@ let searchInput = document.querySelector(".search-input");
 $(".button").click(search);
 
 function search(event) {
-  genre = $(event.target)[0].getInnerHTML();
-  if (genre == "Search") genre = null;
+  
+  genre=$(event.target)[0].getInnerHTML();
+  if(genre == "Search")
+    genre = null;
   searchValue = searchInput.value;
   console.log(searchValue);
   $("#allMovies").empty();
@@ -23,69 +24,44 @@ function search(event) {
 }
 
 function getMovies(genre, searchValue) {
+  
   var finalUrl = "";
-  if (genre != null && searchValue != null && searchValue != "") {
-    finalUrl = imdbGenreUrl + genre + "&title=" + searchValue; //+ "&title_type=feature" ;
-  } else if (genre != null) {
-    finalUrl = imdbGenreUrl + genre; // + "&title_type=feature";
+  if(genre != null && searchValue != null && searchValue != ""){
+    finalUrl = imdbGenreUrl + genre+ "&title=" + searchValue ;//+ "&title_type=feature" ;
+  } else if(genre != null){
+    finalUrl = imdbGenreUrl + genre ;// + "&title_type=feature";
   } else {
-    finalUrl = imdbGenre_title_Url + searchValue; //  + "&title_type=feature";
+    finalUrl = imdbGenre_title_Url + searchValue;//  + "&title_type=feature";
   }
   fetch(finalUrl)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
       displayMovies(data.results);
-    })
+    })  
     .catch(() => console.log("Error"));
 }
 
 function displayMovies(resultsMovie) {
-  for (var i = 1; i < resultsMovie.length; i++) {
-    var card = $("<div>").addClass("card");
-    var title = $("<div>")
-      .addClass("card-header h5")
-      .text(resultsMovie[i].title);
-
-    var figure = $("<figure>");
-    var imgIcon = $("<img>").attr("src", resultsMovie[i].image);
-    figure.append(imgIcon);
-    imgIcon.addClass("imgstyle");
-    var id = resultsMovie[i].id;
-    imgIcon.wrap("<a></a>");
-    var link = $("a").attr({ href: "movies.html" }, { id: resultsMovie[i].id });
-    var genre = $("<p>")
-      .addClass("card-text")
-      .text("Genre : " + resultsMovie[i].genres);
-    var rating = $("<p>")
-      .addClass("card-text")
-      .text("Rating : " + resultsMovie[i].imDbRating);
-    var plot = $("<p>")
-      .addClass("card-text")
-      .text("Plot : " + resultsMovie[i].plot);
-    var cardBody = $("<div>").addClass("card-body");
-    cardBody.append(title, genre, rating, plot, figure);
-    card.append(cardBody);
-    $("#allMovies").append(card);
-    console.log(id);
+  for (var i = 0; i < resultsMovie.length; i++) {
+      var card = $("<div>").addClass("card");      
+      var title = $("<div>").addClass("card-header h5").text(resultsMovie[i].title);
+      
+      var figure = $("<figure>").addClass("image is-128x128");      
+      var imgIcon = $("<img>").attr("src", resultsMovie[i].image);        
+      figure.append(imgIcon);
+      var myId = resultsMovie[i].id;
+      imgIcon.wrap( "<a id='a"+ myId  +"' href=" +  "./movies.html?id="+ myId + "></a>" );
+      
+      //var link = $("#a"+ myId).attr({href:},{id:resultsMovie[i].id});
+      
+      
+      var genre = $("<p>").addClass("card-text").text("Genre : " + resultsMovie[i].genres);
+      var rating = $("<p>").addClass("card-text").text("Rating : " + resultsMovie[i].imDbRating);
+      var plot = $("<p>").addClass("card-text").text("Plot : " + resultsMovie[i].plot);
+      var cardBody = $("<div>").addClass("card-body");
+      cardBody.append(title, genre, rating, plot,figure);
+      card.append(cardBody);
+      $("#allMovies").append(card);
   }
-}
-// imgIcon.addeventlistener("click", )
-// function storeMovie(movie) {
-//   // localStorage.setItem("movie", movie);
-//   console.log(movie);
-// }
-// // let ytApiKey = "AIzaSyDiOCYClrpn3Cz3dIBf7MCWsD-e9KltnIE";
-// // function youtube(searchValue) {
-// //   fetch(
-// //     "https://youtube.googleapis.com/youtube/v3/search?key=" +
-// //       ytApiKey +
-// //       "&type=video&part=snippet&q=" +
-// //       searchValue +
-// //       "trailer"
-// //   )
-// //     .then((response) => response.json())
-// //     .then((data) => {
-// //       console.log(data);
-// //     });
-// // }
+};
