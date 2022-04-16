6dfb6142c0266c765b88ebd340eb4fb4dab610ef
Bhavna Jain <bhavnaostwal@gmail.com>
2022-04-16 14:08:50 -0400

    making previous search links

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # WorthTheWatch
 
+ Seeing that everyone is looking to watch a movie though there are so many choices it is nice to have an easy way to search and look up the movies either by genre or title.
+ 
+ ## User Story
+
+AS a Movie Buff
+I want a website that provides me with all the information I need about a movie
+So THAT I can decide if the movie is WORTH THE WATCH
+## Expected Goals
+
+* If I search for a film or television show by title or genre 
+Then I am presented with search results resembling my search query
+The results will display a brief description of the movie including plot rating and movie poster
+* When I click a movie image i am taken to another page that displays more detailed information about that movie including the movie rating, director, main actors, and a youtube trailer
+* When i click on previous searches a card appears with the previously searched movies 
+
+## Technologies used
+
+* we used the imdb and youtube api to gather data from their servers using api keys
+
+     -[imdb](https://imdb-api.com/api) 
+     -[youtubeApi](https://developers.google.com/youtube/v3)
+
+* we have used Bulma CSS to style our index and movies pages
+ Also we have use the Bulma modal card to display our previous searches
+ 
+* we used local storage to gather and hold the search history to later be used
+     
+## Screenshots
+![Worth The Watch Home](./assets/images/Screenshot-1.png)
+
+
+![Movie Info](./assets/images/Screenshot-2.png)
+
+![WorthTheWatch](./assets/images/Worth-The-Watch.gif)
+
+
+##  Link to the deployed application
+[WorthTheWatch](https://kernbryson.github.io/WorthTheWatch/)
+
+
+    
+## Contributors
+
+* [Bhavna Jain](mailto:Bhavnaostwal@gmail.com)
+    
+* [Bryson Kern](mailto:kernbryson@yahoo.com)
+
+* [Dewey Preast](mailto:Aestheticfoliage@outlook.com)
--- assets/JS/movies.js
+++ assets/JS/movies.js
@@ -8,6 +8,8 @@ let movieActors = document.querySelector(".actorscall");
 let movieScore = document.querySelector(".rewardscall");
 let poster = document.querySelector(".postercall");
 let youtubeVideo = document.querySelector(".youtubemovie");
+let movieLegnth = document.querySelector(".lengthcall");
+let ytApiKey = "AIzaSyDiOCYClrpn3Cz3dIBf7MCWsD-e9KltnIE";
 
 var getUrlParameter = function getUrlParameter(sParam) {
   var sPageURL = window.location.search.substring(1),
@@ -29,18 +31,53 @@ var getUrlParameter = function getUrlParameter(sParam) {
 
 var getTrailerLink = function (myId) {
   var apiUrl = "https://imdb-api.com/en/API/Trailer/k_w0qs6e0f/" + myId;
+  // let movieHistory = JSON.parse(localStorage.getItem("history")) || [];
   fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+      // let movieObject = {
+      //   title: data["title"],
+      //   movieId: data["imDbId"],
+      // };
+      // movieHistory.push(movieObject);
+      populateSearchHistory(data["imDbId"], data["title"]);
       console.log("You clicked" + data.fullTitle);
       title.textContent = data["title"];
-      movieRating.textContent = data["year"];
-      youtubeApi(title.textContent, movieRating.textContent);
-      localStorage.setItem("history", data.fullTitle);
+      releaseDate.textContent = data["year"];
+      youtubeApi(title.textContent, releaseDate.textContent);
+      // localStorage.setItem("history", JSON.stringify(movieHistory));
     })
     .catch(() => console.log("Error"));
 };
+
+// function to store and populate search history
+function populateSearchHistory(imdbId, titleMovie){
+
+  var history = JSON.parse(localStorage.getItem("history"));  
+  var listitem = {
+      Title: titleMovie,
+      MovieId: imdbId,
+    };
+
+  // If exists 
+  if(history){
+
+      for(var i = 0 ; i < history.length; i++){
+          
+          if(history[i].MovieId === imdbId){
+              return;
+          }         
+      } 
+      history.unshift(listitem);        
+  }
+  else{
+          history = [listitem]; 
+  }
+
+  localStorage.setItem("history", JSON.stringify(history));   
+}
+
 
 function getMovieInfo(myId) {
   var apiUrl =
@@ -58,10 +95,11 @@ function getMovieInfo(myId) {
       movieActors.textContent = data["stars"];
       movieRating.textContent = data["contentRating"];
       movieScore.textContent = data["imDbRating"] + "/10";
+      movieLegnth.textContent = data["runtimeStr"];
     })
     .catch(() => console.log("Error"));
 }
-let ytApiKey = "AIzaSyDiOCYClrpn3Cz3dIBf7MCWsD-e9KltnIE";
+
 function youtubeApi(searchValue, year) {
   fetch(
     "https://youtube.googleapis.com/youtube/v3/search?key=" +
@@ -84,4 +122,3 @@ var id = getUrlParameter("id");
 console.log("You clicked" + id);
 getTrailerLink(id);
 getMovieInfo(id);
-
